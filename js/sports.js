b5f414d7c8d0bbb5153fff2fd669d55b7a1d9e77
Rodrigo <rodrigomoura2627@gmail.com>
2026-05-24 20:06:05 -0300

    feat: adicionar dados dos esportes

--- js/sports.js
+++ js/sports.js
@@ -1,1 +1,39 @@
-// Dados dos esportes
+const sports = {
+  volei: {
+    name: "Volei",
+    playersPerTeam: 6
+  },
+  basquete: {
+    name: "Basquete",
+    playersPerTeam: 6
+  },
+  futebol: {
+    name: "Futebol",
+    playersPerTeam: 11
+  }
+};
+
+const samplePlayers = [
+  "Ana",
+  "Bruno",
+  "Carla",
+  "Diego",
+  "Eduarda",
+  "Felipe",
+  "Gabriela",
+  "Henrique",
+  "Isabela",
+  "Joao",
+  "Karen",
+  "Lucas",
+  "Mariana",
+  "Nicolas",
+  "Olivia",
+  "Pedro",
+  "Rafaela",
+  "Samuel",
+  "Talita",
+  "Victor",
+  "Wesley",
+  "Yasmin"
+];
